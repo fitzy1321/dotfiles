e84e56fe62cd796bb345da2df249f84b38cf7756
Joe FItzy <jfitzy1321@protonmail.com>
2020-10-24 10:37:58 -0600

    rearranged files and removed unneeded files

--- scripts/gitignore-reset.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-# rm all files
-git rm -r --cached .
-# add all files as per new .gitignore
-git add .
-# now, commit for new .gitignore to apply
-git commit -m "resetting gitignore cache"

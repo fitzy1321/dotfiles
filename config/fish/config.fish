0da242bfb0d610455ef1e0e33e0bd6c1daedfd17
langnostic <langnostic1@pm.me>
2021-04-08 00:35:40 -0600

    cleaned and reorganized fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,34 +3,43 @@
 #####  Remove fish greeting  #####
 set fish_greeting ""
 
-#####  Setup Dotfiles stuff  #####
+#####  Setup Common Paths  #####
 set SOURCE_DIR $HOME/Source
 set DOTFILES $SOURCE_DIR/dotfiles
+set FISH_PATH $XDG_CONFIG_HOME/fish
+set NVIM_PATH $XDG_CONFIG_HOME/nvim
 
 #####  Aliases  #####
 alias vim='nvim'
 alias ll='ls -lah'
 
 #####  Set Abbreviations  ######
+# Docker
 abbr --add d 'docker'
 abbr --add dps 'docker ps'
 abbr --add dc 'docker-compose'
 abbr --add dcb 'docker-compose build'
 abbr --add dcu 'docker-compose up'
 abbr --add dcdr 'docker-compose down --remove-orphans'
-abbr --add dotfiles 'cd $DOTFILES'
+
+# Edit Common Configs
+abbr --add edit_fish 'vim $FISH_PATH/config.fish'
+abbr --add edit_nvim 'vim $NVIM_PATH/init.vim'
+
+# Updates and Upgrades
 abbr --add dupgrade 'deno upgrade'
-abbr --add edit-fish-config 'vim $XDG_CONFIG_HOME/fish/config.fish'
-abbr --add finstall 'sudo flatpak install flahub'
 abbr --add fupdate 'flatpak update'
-abbr --add nvim-install-plug 'neovim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"'
-abbr --add ppath 'echo $PATH'
 abbr --add rupdate 'rustup update'
-abbr --add source-fish 'source $XDG_CONFIG_HOME/fish/config.fish'
 abbr --add update 'sudo apt update && apt list --upgradable'
 abbr --add upgrade 'sudo apt upgrade -y'
 
-#####  Git abbreviations  #####
+# Misc
+abbr --add dotfiles 'cd $DOTFILES'
+abbr --add install_vimplugs 'neovim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"'
+abbr --add ppath 'echo $PATH'
+abbr --add reset_fish 'source $FISH_PATH/config.fish'
+
+# Git abbr's
 abbr --add ga 'git add'
 abbr --add gaa 'git add -A'
 abbr --add gb 'git branch'
@@ -51,7 +60,7 @@ abbr --add gsl 'git status --long'
 abbr --add gss 'git status -s'
 abbr --add gst 'git status'
 
-####    Setup Deno in Path  #####
+#####  Add Deno to Path  #####
 if test -d $HOME/.local/deno
     set -x DENO_INSTALL $HOME/.local/deno
     fish_add_path $HOME/.local/deno/bin
@@ -60,7 +69,7 @@ else if test -d $HOME/.deno
     fish_add_path $HOME/.deno/bin
 end
     
-#####   Add cargo to fish path, if it exists    #####
+#####  Add Cargo to Path  #####
 if test -d $HOME/.cargo 
     fish_add_path $HOME/.cargo/bin
 end

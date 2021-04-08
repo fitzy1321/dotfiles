11c1e5a4edba1c2da6a9b46baeefdd05fc9ff11b
langnostic <langnostic1@pm.me>
2021-04-07 23:40:32 -0600

    Added more abbrs to fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -20,11 +20,13 @@ abbr --add dcu 'docker-compose up'
 abbr --add dcdr 'docker-compose down --remove-orphans'
 abbr --add dotfiles 'cd $DOTFILES'
 abbr --add dupgrade 'deno upgrade'
+abbr --add edit-fish-config 'vim $XDG_CONFIG_HOME/fish/config.fish'
 abbr --add finstall 'sudo flatpak install flahub'
 abbr --add fupdate 'flatpak update'
 abbr --add nvim-install-plug 'neovim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"'
 abbr --add ppath 'echo $PATH'
 abbr --add rupdate 'rustup update'
+abbr --add source-fish 'source $XDG_CONFIG_HOME/fish/config.fish'
 abbr --add update 'sudo apt update && apt list --upgradable'
 abbr --add upgrade 'sudo apt upgrade -y'
 
@@ -40,6 +42,7 @@ abbr --add gcb 'git checkout -b'
 abbr --add gcm 'git commit -m'
 abbr --add gco 'git checkout'
 abbr --add gd 'git diff'
+abbr --add gds 'git diff --staged'
 abbr --add gl 'git pull'
 abbr --add gp 'git push'
 abbr --add gpu 'git push -u origin'
@@ -67,6 +70,6 @@ if test (uname) = "Darwin"
     alias updatedb="sudo /usr/libexec/locate.updatedb"
 end
 
-#####  Start Starship for Fish  #####
+#####  Starship Prompt setup  #####
 starship init fish | source
 

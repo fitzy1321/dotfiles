058691b7d58f70364fb87ebbc2058681767e91b2
joefitzy <90652069+joefitzy@users.noreply.github.com>
2022-07-12 16:19:57 -0600

    fish config updates, and add to stowrc ignore list

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,5 @@
-# Pro-tip: use fish_add_path to edit the PATH variable in this config file
 # DO NOT MANUALLY EDIT $PATH IN THIS FILE!!!!
-# etc: fish_add_path $HOME/.cargo/bin
+# use `fish_add_path` instead: fish_add_path $HOME/.cargo/bin
 
 # for nvm, install with fisher
 # `fisher install FabioAntunes/fish-nvm edc/bass`
@@ -11,9 +10,11 @@ set -g fish_greeting
 #### Theme
 set -Ux theme_nerd_fonts yes
 
-#### Add ENV vars
+#### XDG variables
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
+
+#### Custom variables
 set -gx FISH_PATH $XDG_CONFIG_HOME/fish
 set -gx MYVIMRC $XDG_CONFIG_HOME/nvim/init.vim
 set -gx DOTFILES $HOME/.dotfiles
@@ -24,7 +25,7 @@ if test (uname) = "Linux"
 end
 
 #### macOS Specific Configs
-if test (uname) = "Darwin"
+if test (uname -s) = "Darwin"
     alias updatedb="sudo /usr/libexec/locate.updatedb"
     if test -e /opt/homebrew/bin; or test -d /opt/homebrew/bin
         fish_add_path /opt/homebrew/bin
@@ -36,17 +37,21 @@ if test (uname) = "Darwin"
 end
 
 #### Add Paths
-if test -d /usr/local/bin; and not contains /usr/local/bin $PATH
-    fish_add_path /usr/local/bin
-end
+test -d /usr/local/bin; and fish_add_path /usr/local/bin
 test -d $HOME/.local/bin; and fish_add_path $HOME/.local/bin
 test -d $HOME/bin; and fish_add_path $HOME/bin
 
+#### Rust / Cargo
 test -d $HOME/.cargo; and fish_add_path $HOME/.cargo/bin
+
+#### Deno setup
+if test -d $HOME/.Deno
+    set -q DENO_INSTALL; or set -gx DENO_INSTALL $HOME/.deno
+    fish_add_path $DENO_INSTALL/bin
+end
 
 #### Aliases
 type -q exa >/dev/null; and alias ls exa
-type -q bat >/dev/null; and alias cat bat
 
 #### Set Abbreviations
 if test -d $HOME/Source/vukaheavy

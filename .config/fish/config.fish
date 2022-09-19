58951c94b259af7b6d5aa6bf84aa3d142e481157
joefitzy <90652069+joefitzy@users.noreply.github.com>
2022-09-19 10:45:56 -0600

    fix comment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -94,7 +94,7 @@ end
 abbr -a ppath 'echo $PATH'
 abbr -a reset_fish 'source $FISH_PATH/config.fish'
 
-# My virtualenv setup command, yes pyenv integration without a wrapper
+# My virtualenv setup command, easy pyenv integration without a wrapper
 type -q virtualenv >/dev/null; and abbr -a venv 'virtualenv -p (pyenv version-name) .venv'; or abbr -e venv
 
 # Git abbr's

86a372abbc5284c3ede9db20cc033ec90cb2f904
Joe Fitzgibbons <90652069+joefitzy@users.noreply.github.com>
2022-12-02 17:47:33 -0700

    Create clean_venv.fish

---
+++ .config/fish/functions/clean_venv.fish
@@ -0,0 +1,18 @@
+function clean_venv --description="Delete current virtualenv and create a new one with virtualenv and pyenv"
+  if ! type -q virtualenv
+    echo "Please install virtualenv"
+    return 1
+  end
+
+  if ! type -q pyenv
+    echo "please install pyenv"
+  end
+
+  if ! test -d .venv
+    echo ".venv directory not found!"
+    return 1
+  end
+
+  rm -rf .venv && virtualenv -p (pyenv version-name) .venv && source .venv/bin/activate.fish && pip install --upgrade pip && deactivate
+  return 0
+end

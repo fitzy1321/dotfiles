eaec531eba91c9d9167ce652abc76a46c77aee55
fitzypop <32967490+fitzypop@users.noreply.github.com>
2022-12-12 10:56:56 -0700

    sync fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,52 +1,16 @@
 # Fitypop's Fish Config
 
 # DO NOT MANUALLY EDIT $PATH IN THIS FILE!!!!
-# etc: fish_add_path $HOME/.cargo/bin
-
-#### Remove fish greeting
-set -g fish_greeting
-
-#### Setup themes
-set -Ux theme_nerd_fonts yes
+# use `fish_add_path` instead: fish_add_path $HOME/.cargo/bin
 
 #### XDG variables
 set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME $HOME/.config
 set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME $HOME/.cache
 
-#### Go / g (go manager) variables
-set -gx GOPATH $HOME/go
-set -gx GOROOT $HOME/.go
-set -gx PATH $GOPATH/bin $PATH
-# g-install: do NOT edit, see https://github.com/stefanmaric/g
-
-#### Custom variables
-set -q FISH_PATH; or set -gx FISH_PATH $XDG_CONFIG_HOME/fish
-set -q MYVIMRC; or set -gx MYVIMRC $XDG_CONFIG_HOME/nvim/init.vim
-set -q DOTFILES; or set -gx DOTFILES $HOME/.dotfiles
-set -q PYTHONSTARTUP; or set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
-
-# macOS Specific thangs
-if test (uname -s) = Darwin
-    alias updatedb='sudo /usr/libexec/locate.updatedb'
-    # homebrew setup
-    test -e /opt/homebrew/bin; or test -d /opt/homebrew/bin; and fish_add_path /opt/homebrew/bin
-    # iterm2 setup
-    status is-interactive; and test "$TERM_PROGRAM" = "iTerm.app"; and source $FISH_PATH/iterm2.fish
-else
-    if test -e /usr/bin/fish; and test ! -e /usr/local/bin/fish
-        echo 'Need to make a symlink so fish and alacritty can work together.'
-        ln -s /usr/bin/fish /usr/local/bin/fish
-    end
-end
-
 #### Setup paths
-# not contains /usr/local/bin $PATH; and fish_add_path /usr/local/bin
-# not contains $HOME/.local/bin $PATH; and test -d $HOME/.local/bin; and fish_add_path $HOME/.local/bin
-# not contains $HOME/bin; and test -d $HOME/bin; and fish_add_path $HOME/bin
 test -d /usr/local/bin; and fish_add_path /usr/local/bin
 test -d $HOME/.local/bin; and fish_add_path $HOME/.local/bin
 test -d $HOME/bin; and fish_add_path $HOME/bin
-
 
 # Rust / Cargo
 test -d $HOME/.cargo; and fish_add_path $HOME/.cargo/bin
@@ -57,97 +21,134 @@ if test -d $HOME/.deno
     fish_add_path $DENO_INSTALL/bin
 end
 
-#### Aliases
-
-#### Set Abbreviations
-# Docker
-abbr --add d docker
-abbr --add dc docker-compose
-abbr --add dcdr 'docker-compose down --remove-orphans'
-
-# Linux Specific things
-if test (uname) = Linux
-    abbr --add update 'sudo apt update && apt list --upgradable'
-    abbr --add upgrade 'sudo apt upgrade -y'
-    abbr --add fupdate 'flatpak update'
-    abbr --add flatpak_unused 'flatpak uninstall --unused'
-end
-
-# Misc
-abbr --add dotfiles 'cd $DOTFILES'
-abbr --add install_vimplugs 'nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"'
-
-if type -q exa
-    abbr --add ls 'exa --icons --group-directories-first'
-    abbr --add la 'exa -a --icons --group-directories-first'
-    abbr --add ll 'exa -la --icons --group-directories-first'
-    abbr --add lt 'exa -la --icons --group-directories-first --tree --level=2'
-else
-    abbr --add ll 'ls -lhAF'
-end
-
-abbr -a pre prevd # shorthand for previous directory
-abbr -a rfish 'source $FISH_PATH/config.fish'
-
-# My virtualenv setup command, easy pyenv integration without a wrapper
-if type -q virtualenv
-    abbr -a venv 'virtualenv -p (pyenv version-name) .venv'
-else
-    abbr -e venv
-end
-
-# Git abbr's
-abbr -a ga 'git add'
-abbr -a gaa 'git add -A'
-
-abbr -a gb 'git branch'
-
-abbr -a gcm 'git commit -m'
-abbr -a gcam 'git commit -am'
-
-abbr -a gch 'git checkout'
-abbr -a gchb 'git checkout -b'
-abbr -a gchpoetry 'git checkout master -- poetry.lock'
-
-abbr -a gd 'git diff'
-abbr -a gds 'git diff --staged'
-
-# git diffs without lock files
-abbr -a gdnl "git diff -- ':!*Cargo.lock' ':!*poetry.lock'"
-abbr -a gdsnl "git diff --staged -- ':!*Cargo.lock' ':!*poetry.lock'"
-
-abbr -a ggd 'git log --graph --oneline --decorate'
-
-abbr -a gl 'git pull'
-abbr -a glv 'git pull && git diff ORIG_HEAD..'
-
-abbr -a gp 'git push'
-abbr -a gpu 'git push -u origin'
-abbr -a gpum 'git push -u origin main'
-
-abbr -a gst 'git status'
-abbr -a gsl 'git status --long'
-abbr -a gss 'git status -s'
-
-# Github CLI shortcuts
-if type -q gh
-    abbr -a gprv 'gh pr view -w'
-    abbr -a grv 'gh repo view -w'
-else
-    abbr -e gprv
-    abbr -e grv
-end
-
-#### Pyenv setup
-if test -d $HOME/.pyenv
-    set -q PYENV_ROOT; or set -gx PYENV_ROOT $HOME/.pyenv
-    fish_add_path $PYENV_ROOT/bin
-    status is-login; and pyenv init --path | source
-    status is-interactive; and pyenv init - | source
-end
-
 if status is-interactive
+    #### Remove fish greeting
+    set -g fish_greeting
+
+    #### Setup themes
+    set -Ux theme_nerd_fonts yes
+
+    #### Go / g (go manager) variables
+    set -gx GOPATH $HOME/go
+    set -gx GOROOT $HOME/.go
+    set -gx PATH $GOPATH/bin $PATH
+    # g-install: do NOT edit, see https://github.com/stefanmaric/g
+
+    #### Custom variables
+    set -q FISH_PATH; or set -gx FISH_PATH $XDG_CONFIG_HOME/fish
+    set -q MYVIMRC; or set -gx MYVIMRC $XDG_CONFIG_HOME/nvim/init.vim
+    set -q DOTFILES; or set -gx DOTFILES $HOME/.dotfiles
+    set -q PYTHONSTARTUP; or set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
+
+    # macOS Specific thangs
+    if test (uname -s) = Darwin
+        alias updatedb='sudo /usr/libexec/locate.updatedb'
+        # homebrew setup
+        test -e /opt/homebrew/bin; or test -d /opt/homebrew/bin; and fish_add_path /opt/homebrew/bin
+        # iterm2 setup
+        # test "$TERM_PROGRAM" = "iTerm.app"; and source $FISH_PATH/iterm2.fish
+    else
+        # Alacritty trouble shooting
+        # if test -e /usr/bin/fish; and test ! -e /usr/local/bin/fish
+        #     echo 'Need to make a symlink so fish and alacritty can work together.'
+        #     ln -s /usr/bin/fish /usr/local/bin/fish
+        # end
+    end
+
+    #### Aliases
+
+    #### Set Abbreviations
+    # Docker
+    abbr --add d docker
+    abbr --add dc docker-compose
+    abbr --add dcdr 'docker-compose down --remove-orphans'
+
+    # Linux Specific things
+    if test (uname) = Linux
+        abbr --add update 'sudo apt update && apt list --upgradable'
+        abbr --add upgrade 'sudo apt upgrade -y'
+        abbr --add fupdate 'flatpak update'
+        abbr --add flatpak_unused 'flatpak uninstall --unused'
+    end
+
+    # Misc
+    abbr --add dotfiles 'cd $DOTFILES'
+    abbr --add install_vimplugs 'nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"'
+
+    if type -q exa
+        abbr --add ls 'exa --icons --group-directories-first'
+        abbr --add la 'exa -a --icons --group-directories-first'
+        abbr --add ll 'exa -la --icons --group-directories-first'
+        abbr --add lt 'exa -la --icons --group-directories-first --tree --level=2'
+    else
+        abbr --add ll 'ls -lhAF'
+    end
+
+    abbr -a pre prevd # shorthand for previous directory
+    abbr -a rfish 'source $FISH_PATH/config.fish'
+
+    # My virtualenv setup command, easy pyenv integration without a wrapper
+    if type -q virtualenv
+        abbr -a venv 'virtualenv -p (pyenv version-name) .venv'
+    else
+        abbr -e venv
+    end
+
+    # Git abbr's
+    abbr -a ga 'git add'
+    abbr -a gaa 'git add -A'
+
+    abbr -a gb 'git branch'
+
+    abbr -a gcm 'git commit -m'
+    abbr -a gcam 'git commit -am'
+
+    abbr -a gch 'git checkout'
+    abbr -a gchb 'git checkout -b'
+    abbr -a gchpoetry 'git checkout master -- poetry.lock'
+
+    abbr -a gd 'git diff'
+    abbr -a gds 'git diff --staged'
+
+    # git diffs without lock files
+    set -q _git_ignore_list; or set _git_ignore_list "':!*Cargo.lock' ':!*package-lock.json' ':!*poetry.lock' ':!*yarn.lock'"
+    abbr -a gdnl git diff -- $_git_ignore_list
+    abbr -a gdsnl git diff --staged -- $_git_ignore_list
+
+    abbr -a ggd 'git log --graph --oneline --decorate'
+
+    abbr -a gl 'git pull'
+    abbr -a glv 'git pull && git diff ORIG_HEAD..'
+
+    abbr -a gp 'git push'
+    abbr -a gpu 'git push -u origin'
+    abbr -a gpum 'git push -u origin main'
+
+    abbr -a gst 'git status'
+    abbr -a gsl 'git status --long'
+    abbr -a gss 'git status -s'
+
+    # Github CLI shortcuts
+    if type -q gh
+        abbr -a gprv 'gh pr view -w'
+        abbr -a grv 'gh repo view -w'
+
+    else
+        abbr -e gprv
+        abbr -e grv
+    end
+
+    #### Pyenv setup
+    if test -d $HOME/.pyenv
+        set -q PYENV_ROOT; or set -gx PYENV_ROOT $HOME/.pyenv
+        fish_add_path $PYENV_ROOT/bin
+        status is-login; and pyenv init --path | source
+        status is-interactive; and pyenv init - | source
+    end
+
+    # Direnv setup
     type -q direnv; and direnv hook fish | source
 
+    # Starship prompt setup
     type -q starship; and starship init fish | source
 end

af1ff2673e8305a11464cced6b59a3d4ff5eb2bf
fitzypop <32967490+fitzypop@users.noreply.github.com>
2022-12-27 12:37:44 -0700

    clean up fish config and .profile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,14 +3,12 @@
 # DO NOT MANUALLY EDIT $PATH IN THIS FILE!!!!
 # use `fish_add_path` instead: fish_add_path $HOME/.cargo/bin
 
-#### XDG variables
+set -gx PYTHONDONTWRITEBYTECODE 1 # prevent .pyc files
+set -gx SHELL fish
+
+# XDG variables
 set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME $HOME/.config
 set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME $HOME/.cache
-
-#### Setup paths
-test -d /usr/local/bin; and fish_add_path /usr/local/bin
-test -d $HOME/.local/bin; and fish_add_path $HOME/.local/bin
-test -d $HOME/bin; and fish_add_path $HOME/bin
 
 # Rust / Cargo
 test -d $HOME/.cargo; and fish_add_path $HOME/.cargo/bin
@@ -22,23 +20,29 @@ if test -d $HOME/.deno
 end
 
 if status is-interactive
-    #### Remove fish greeting
+    # Remove fish greeting
     set -g fish_greeting
 
-    #### Setup themes
+    # Setup themes
     set -Ux theme_nerd_fonts yes
 
-    #### Go / g (go manager) variables
+    # Go / g (go manager) variables
+    # g-install: do NOT edit, see https://github.com/stefanmaric/g
     set -gx GOPATH $HOME/go
     set -gx GOROOT $HOME/.go
-    set -gx PATH $GOPATH/bin $PATH
-    # g-install: do NOT edit, see https://github.com/stefanmaric/g
+    fish_add_path $GOPATH/bin
 
-    #### Custom variables
+    # Custom variables
+    set -q EDITOR; or set -gx EDITOR nvim
     set -q FISH_PATH; or set -gx FISH_PATH $XDG_CONFIG_HOME/fish
     set -q MYVIMRC; or set -gx MYVIMRC $XDG_CONFIG_HOME/nvim/init.vim
     set -q DOTFILES; or set -gx DOTFILES $HOME/.dotfiles
-    set -q PYTHONSTARTUP; or set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
+    # set -q PYTHONSTARTUP; or set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
+
+    # Setup paths
+    test -d /usr/local/bin; and fish_add_path /usr/local/bin
+    test -d $HOME/.local/bin; and fish_add_path $HOME/.local/bin
+    test -d $HOME/bin; and fish_add_path $HOME/bin
 
     # macOS Specific thangs
     if test (uname -s) = Darwin
@@ -55,37 +59,41 @@ if status is-interactive
         # end
     end
 
-    #### Aliases
+    # Set Abbreviations
 
-    #### Set Abbreviations
+    # faster traversal up dirs
+    abbr -a ... "../../"
+    abbr -a .... "../../../"
+    abbr -a ..... "../../../../"
+    abbr -a ...... "../../../../../"
+
     # Docker
-    abbr --add d docker
-    abbr --add dc docker-compose
-    abbr --add dcdr 'docker-compose down --remove-orphans'
+    abbr -a d docker
+    abbr -a dc docker-compose
+    abbr -a dcdr 'docker-compose down --remove-orphans'
 
     # Linux Specific things
     if test (uname) = Linux
-        abbr --add update 'sudo apt update && apt list --upgradable'
-        abbr --add upgrade 'sudo apt upgrade -y'
-        abbr --add fupdate 'flatpak update'
-        abbr --add flatpak_unused 'flatpak uninstall --unused'
+        abbr -a update 'sudo apt update && apt list --upgradable'
+        abbr -a upgrade 'sudo apt upgrade -y'
+        abbr -a fupdate 'flatpak update'
+        abbr -a flatpak_unused 'flatpak uninstall --unused'
     end
 
     # Misc
-    abbr --add dotfiles 'cd $DOTFILES'
-    abbr --add install_vimplugs 'nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"'
-
-    if type -q exa
-        abbr --add ls 'exa --icons --group-directories-first'
-        abbr --add la 'exa -a --icons --group-directories-first'
-        abbr --add ll 'exa -la --icons --group-directories-first'
-        abbr --add lt 'exa -la --icons --group-directories-first --tree --level=2'
-    else
-        abbr --add ll 'ls -lhAF'
-    end
-
+    abbr -a dotfiles 'cd $DOTFILES'
+    abbr -a install_vimplugs 'nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"'
     abbr -a pre prevd # shorthand for previous directory
     abbr -a rfish 'source $FISH_PATH/config.fish'
+
+    if type -q exa
+        abbr -a ls 'exa --icons --group-directories-first'
+        abbr -a la 'exa -a --icons --group-directories-first'
+        abbr -a ll 'exa -la --icons --group-directories-first'
+        abbr -a lt 'exa -la --icons --group-directories-first --tree --level=2'
+    else
+        abbr -a ll 'ls -lhAF'
+    end
 
     # My virtualenv setup command, easy pyenv integration without a wrapper
     if type -q virtualenv
@@ -138,7 +146,9 @@ if status is-interactive
         abbr -e grv
     end
 
-    #### Pyenv setup
+    # Tooling / prompts
+
+    # Pyenv setup
     if test -d $HOME/.pyenv
         set -q PYENV_ROOT; or set -gx PYENV_ROOT $HOME/.pyenv
         fish_add_path $PYENV_ROOT/bin
--- .profile
+++ .profile
@@ -9,14 +9,12 @@ export LESSHISTFILE=-
 export HISTFILE="$XDG_CACHE_HOME/bash_history"
 
 # Remove Python History file with this pythonrc config
-export PYTHONSTARTUP="$XDG_CONFIG_HOME/pythonrc"
+export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/pythonrc"
 
 # Aliases used across shells
 # not include 'l' alias, because i want to use fish abbr for it
 alias ls="ls --color=auto"
 alias wget="wget --hsts-file $XDG_CACHE_HOME/wget_history"
-export EDITOR=nvim
-export MYVIMRC="$XDG_CONFIG_HOME/nvim/init.vim"
 
 # if running bash
 if [ -n "$BASH_VERSION" ]; then
@@ -25,10 +23,3 @@ if [ -n "$BASH_VERSION" ]; then
 	. "$HOME/.bashrc"
     fi
 fi
-
-# set cargo path
-[ -d "$HOME/.cargo" ] && PATH="$HOME/.cargo/bin:$PATH"
-
-# set user's bin in PATH, if it exists
-[ -d "$HOME/.local/bin" ] && PATH="$HOME/.local/bin:$PATH"
-[ -d "$HOME/bin" ] && PATH="$HOME/bin:$PATH"

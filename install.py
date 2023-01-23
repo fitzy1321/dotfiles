2549a82e8e7914557fd3a37e35f6a11fc4ccf940
Joe Fitzgibbons <langnostic1@pm.me>
2023-01-23 00:31:14 -0700

    iterating on install script

--- install.py
+++ install.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+"""This script will install and setup Fitzypop's latest dotfiles, dev workflow, and system packages."""
+import argparse
 import os
 import subprocess
 import sys
@@ -8,6 +11,14 @@ SHELL = os.getenv("SHELL", "")
 WINDOWS = sys.platform.startswith("win") or (sys.platform == "cli" and os.name == "nt")
 
 
+XCODE_CMD = "xcode-select --install"
+BREW_CMD = 'bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+
+# Various installation paths
+BREW_PATH_DEFAULT = "/opt/homebrew/bin/"
+DOTFILES_PATH = "~/.dotfiles"
+
+
 def in_file(filenane, substring):
     """Return True if substring is contained with the named file"""
     with open(filenane, "r") as fp:
@@ -15,33 +26,32 @@ def in_file(filenane, substring):
         return substring in data
 
 
-def mac_installer() -> int:
-    xcode_cmd = "xcode-select --install"
-    brew_cmd = 'bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+def mac_installer(args) -> int:
+    brew_path = args.brew_path if args.brew_path else BREW_PATH_DEFAULT
+    dot_path = args.path if args.path else DOTFILES_PATH
+    fish_path = f"{brew_path}fish"
 
-    print(f'installing dev tools with "{xcode_cmd}"')
-    subprocess.run(xcode_cmd, shell=True)
+    print(f'installing dev tools with "{XCODE_CMD}"')
+    subprocess.run(XCODE_CMD, shell=True)
 
     print("installing homebrew")
-    subprocess.run(brew_cmd, shell=True)
+    subprocess.run(BREW_CMD, shell=True)
 
     print("installing necessary packages with brew")
     subprocess.run("brew install git just", shell=True)
 
     # clone dotfiles repo
-    subprocess.run("git clone git@github.com:fitzypop/dotfiles.git $HOME/.dotfiles")
+    subprocess.run(
+        f"{brew_path}git clone git@github.com:fitzypop/dotfiles.git {dot_path}"
+    )
 
     print("Installing remaining brew packages")
-    subprocess.run("just brewinstall", shell=True)
+    subprocess.run(f"{brew_path}just brewinstall", shell=True)
 
     print("installing virtualenv")
-    subprocess.run("pipx install virutalenv", shell=True)
+    subprocess.run(f"{brew_path}pipx install virutalenv", shell=True)
 
     print("Adding fish shell to /etc/shells")
-
-    # with open("/etc/shells", "a") as myfile:
-    #     myfile.write("/opt/homebrew/bin/fish")
-    fish_path = subprocess.run("which fish", shell=True).stdout.decode()
 
     if not in_file("/etc/shells", fish_path):
         # subprocess hack: drop into bash shell with root permissions
@@ -63,8 +73,22 @@ def main() -> int:
         )
         return 1
 
+    parser = argparse.ArgumentParser(
+        description="Installs the latest version of Fitzypop's dotfiles"
+    )
+    parser.add_argument(
+        "--path", dest="path", action="store", help="Install dotfiles to a given path"
+    )
+    parser.add_argument(
+        "--brew-path",
+        dest="brew_path",
+        action="store",
+        help=f"Path to Homebrew bin directory, defautls to {BREW_CMD}",
+    )
+    args = parser.parse_args()
+
     if MACOS:
-        return mac_installer()
+        return mac_installer(args)
 
     return 2
 

9d0ec2de29aec89f28b9900c7167eb0fdc04e765
Joseph Fitzgibbons <32967490+fitzypop@users.noreply.github.com>
2023-01-31 19:17:44 -0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,7 @@
 
 # Remove fish greeting
 set -g fish_greeting
-
 set -Ux theme_nerd_fonts yes
-
 set -gx PYTHONDONTWRITEBYTECODE 1 # prevent .pyc files
 set -gx SHELL fish
 
@@ -13,16 +11,16 @@ set -gx SHELL fish
 set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME $HOME/.config
 set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME $HOME/.cache
 
-# Rust / Cargo
-test -d $HOME/.cargo; and fish_add_path $HOME/.cargo/bin
-
-# Deno setup
-if test -d $HOME/.deno
-    set -q DENO_INSTALL; or set -gx DENO_INSTALL $HOME/.deno
-    fish_add_path $DENO_INSTALL/bin
-end
-
 if status is-interactive
+    # Rust / Cargo
+    test -d $HOME/.cargo; and fish_add_path $HOME/.cargo/bin
+
+    # Deno setup
+    if test -d $HOME/.deno
+        set -q DENO_INSTALL; or set -gx DENO_INSTALL $HOME/.deno
+        fish_add_path $DENO_INSTALL/bin
+    end
+    
     # Go / g (go manager) variables
     # g-install: do NOT edit, see https://github.com/stefanmaric/g
     set -gx GOPATH $HOME/go
@@ -106,14 +104,16 @@ if status is-interactive
     abbr -a ga 'git add'
     abbr -a gaa 'git add -A'
 
-    abbr -a gb 'git branch'
+    abbr -a gb 'git branch -l'
+    abbr -a gbr 'git branch -r'
+    abbr -a gbv 'git branch -vl'
+
+    abbr -a gcb 'git checkout -b'
+    abbr -a gch 'git checkout'
+    abbr -a gchpoetry 'git checkout master -- poetry.lock'
 
     abbr -a gcm 'git commit -m'
     abbr -a gcam 'git commit -am'
-
-    abbr -a gch 'git checkout'
-    abbr -a gchb 'git checkout -b'
-    abbr -a gchpoetry 'git checkout master -- poetry.lock'
 
     abbr -a gd 'git diff'
     abbr -a gds 'git diff --staged'
@@ -132,9 +132,9 @@ if status is-interactive
     abbr -a gpu 'git push -u origin'
     abbr -a gpum 'git push -u origin main'
 
+    abbr -a gs 'git status -s'
     abbr -a gst 'git status'
     abbr -a gsl 'git status --long'
-    abbr -a gss 'git status -s'
 
     # Github CLI shortcuts
     if type -q gh

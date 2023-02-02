5a8af69d6f54c343d724a7f30eb3b7b4c1ca1299
Joe Fitzgibbons <langnostic1@pm.me>
2023-02-01 20:00:09 -0700

    fix clean_venv function

--- .config/fish/functions/clean_venv.fish
+++ .config/fish/functions/clean_venv.fish
@@ -1,18 +1,22 @@
 function clean_venv --description="Delete current virtualenv and create a new one with virtualenv and pyenv"
-  if ! type -q virtualenv
-    echo "Please install virtualenv"
-    return 1
-  end
+    if ! type -q virtualenv
+        echo "Please install virtualenv"
+        return 1
+    end
 
-  if ! type -q pyenv
-    echo "please install pyenv"
-  end
+    if ! type -q pyenv
+        echo "please install pyenv"
+    end
 
-  if ! test -d .venv
-    echo ".venv directory not found!"
-    return 1
-  end
+    if ! test -d .venv
+        echo ".venv directory not found!"
+        return 1
+    end
 
-  rm -rf .venv && virtualenv -p (pyenv version-name) .venv && source .venv/bin/activate.fish && pip install --upgrade pip && deactivate
-  return 0
+    rm -rf .venv
+    virtualenv -p (pyenv version-name) .venv
+    source .venv/bin/activate.fish
+    pip install -U pip black isort pytest ruff
+    deactivate
+    return 0
 end

964d125f687877b9636d08c2344de1473b662cf9
Joseph Fitzgibbons <32967490+fitzypop@users.noreply.github.com>
2023-02-12 21:51:55 -0700

    Update pomw.fish

--- .config/fish/functions/pomw.fish
+++ .config/fish/functions/pomw.fish
@@ -1,7 +1,14 @@
-function pomw --description "Bashbunni's pomodoro mac terminal timer"
-    # From bashbunni's pomadoro timer: https://gist.github.com/bashbunni/f6b04fc4703903a71ce9f70c58345106
-    # Mac setup for pomo
-    timer 60m && terminal-notifier -message Pomodoro \
+# From bashbunni's pomadoro timer: https://gist.github.com/bashbunni/f6b04fc4703903a71ce9f70c58345106
+# Mac setup for pomo
+#  brew install caarlos0/tap/timer terminal-notifier
+
+function pomw --description "Bashbunni's pomodoro mac terminal timer" --argument-names wtimer
+    # set default timer length
+    if test -z $wtimer
+        set -f wtimer 60m
+    end
+
+    timer $wtimer && terminal-notifier -message Pomodoro \
         -title 'Work Timer is up! Take a Break 😊' \
         -appIcon '~/Pictures/pumpkin.png' \
         -sound Crystal

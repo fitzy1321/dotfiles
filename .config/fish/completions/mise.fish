977b5412099c2a459eddb9a4e4cc7c9ac0ea434a
fitzy1321 <32967490+fitzy1321@users.noreply.github.com>
2025-08-21 00:18:05 -0600

    add and remove shell completions

--- .config/fish/completions/mise.fish
+++ .config/fish/completions/mise.fish
@@ -1,10 +1,17 @@
 # if "usage" is not installed show an error
-if ! command -v usage &>/dev/null
+if ! command -v usage &> /dev/null
     echo >&2
     echo "Error: usage CLI not found. This is required for completions to work in mise." >&2
     echo "See https://usage.jdx.dev for more information." >&2
     return 1
 end
 
-set _usage_spec_mise (mise usage | string collect)
-complete -xc mise -a '(usage complete-word --shell fish -s "$_usage_spec_mise" -- (commandline -cop) (commandline -t))'
+if ! set -q _usage_spec_mise_2025_8_13
+  set -g _usage_spec_mise_2025_8_13 (mise usage | string collect)
+end
+set -l tokens
+if commandline -x >/dev/null 2>&1
+    complete -xc mise -a '(usage complete-word --shell fish -s "$_usage_spec_mise_2025_8_13" -- (commandline -xpc) (commandline -t))'
+else
+    complete -xc mise -a '(usage complete-word --shell fish -s "$_usage_spec_mise_2025_8_13" -- (commandline -opc) (commandline -t))'
+end

e398cc3c54e9117168b50dbc4a555cf9a66f6260
fitzy1321 <32967490+fitzy1321@users.noreply.github.com>
2026-07-31 19:15:56 -0600

    update python module string, and add typer completions

---
+++ .config/fish/completions/typer.fish
@@ -0,0 +1,1 @@
+complete --command typer --no-files --arguments "(env _TYPER_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) typer)" --condition "env _TYPER_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) typer"
